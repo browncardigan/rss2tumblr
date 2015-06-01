b08699febc4c6bf7587bc235e8b2c57ddedc8409
browncardigan <jonathan.nail@gmail.com>
2015-06-01 17:05:01 +1000

    _conf_sample.php

---
+++ _conf_sample.php
@@ -0,0 +1,8 @@
+<?php
+define("API_KEY", "0LI4OG5VvuDAFNLglxVUhqCRotN5w1fd7B4lgwFGD2UEHYApUQ");
+define("API_SECRET", "e9j1dA4tB7QVaHYmgTbncZHvyEaC9dbLUv071zttS2897qCcSp");
+define("CALLBACK_URL", "http://static.rollingstoneaus.com/tumblr/callback.php");
+define("TUMBLR_ID", "rollingstoneaus");
+define("RSS_URL", "http://rollingstoneaus.com/rss");
+define("STORED_FILE", "stored/db.txt");
+?>
